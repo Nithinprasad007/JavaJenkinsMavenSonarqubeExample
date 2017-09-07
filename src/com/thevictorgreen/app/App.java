58aa5fd189ccd12ce2f644fb7077d9e87ce87495
thevictorgreen <thevictorgreen@gmail.com>
2017-09-07 13:21:48 -0400

    added initial project files

---
+++ src/com/thevictorgreen/app/App.java
@@ -0,0 +1,15 @@
+//App.java
+package com.thevictorgreen.app;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+
+public class App {
+
+  private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+  public static void main(String[] args) {
+    LOGGER.info("Logger Name: "+LOGGER.getName());
+  }
+
+}
